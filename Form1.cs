520dbbb68fff33a4f5af143d1db12adbc981a3ef
Manuel Soto <sotoj7@outlook.com>
2022-03-18 11:23:31 -0600

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulacionParcial
+{
+    public partial class Form1 : Form
+    {
+        List<Temperatura> temperaturas = new List<Temperatura>();
+        List<Departamento> departamentos = new List<Departamento>();
+        private void CargarDepartamentos(string file)
+        {
+            FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            while (sr.Peek() != -1)
+            {
+                Departamento departamento = new Departamento()
+                {
+                    id = Convert.ToInt32(sr.ReadLine()),
+                    nombre = sr.ReadLine(),
+                };
+                departamentos.Add(departamento);
+            }
+            sr.Close();
+            fs.Close();
+            
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Cargar departamentos en combobox
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarDepartamentos("Departamentos.txt");
+            foreach(Departamento departamento in departamentos)
+            {
+                comboBox1.Items.Add(departamento.nombre);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre = comboBox1.SelectedItem.ToString();
+            Departamento departamento = departamentos.Find(d => d.nombre == nombre);
+            Temperatura temperatura = new Temperatura()
+            {
+                fecha = dateTimePicker1.Value,
+                magnitud = Convert.ToInt32(textBox1.Text),
+                id = Convert.ToInt32(departamento.id)
+            };
+            temperaturas.Add(temperatura);
+            GuardarDatos("Temperaturas.txt");
+        }
+        private void GuardarDatos(string file)
+        {
+            FileStream fs = new FileStream(file, FileMode.Create, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach(Temperatura temperatura in temperaturas)
+            {
+                sw.WriteLine(temperatura.id);
+                sw.WriteLine(temperatura.magnitud);
+                sw.WriteLine(temperatura.fecha);
+            }
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
