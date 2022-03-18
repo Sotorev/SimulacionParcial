aaff788f414da62617c97112f90c73961e97afa3
Manuel Soto <sotoj7@outlook.com>
2022-03-18 11:44:19 -0600

    2

--- Form1.cs
+++ Form1.cs
@@ -72,5 +72,11 @@ namespace SimulacionParcial
             sw.Close();
             fs.Close();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
     }
 }
---
+++ Form2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulacionParcial
+{
+    public partial class Form2 : Form
+    {
+        List<Datos> datos = new List<Datos>();
+        List<Departamento> departamentos = new List<Departamento>();
+        List<Temperatura> temperaturas = new List<Temperatura>();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        void LeerDatos()
+        {
+            FileStream fs = new FileStream("Departamentos.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            while(sr.Peek() != -1)
+            {
+                Departamento departamento = new Departamento()
+                {
+                    id = Convert.ToInt32(sr.ReadLine()),
+                    nombre = sr.ReadLine()
+                };
+                departamentos.Add(departamento);
+            }
+            sr.Close();
+            fs.Close();
+            fs = new FileStream("Temperaturas.txt", FileMode.Open, FileAccess.Read);
+            sr = new StreamReader(fs);
+            while(sr.Peek()!= -1)
+            {
+                Temperatura temperatura = new Temperatura()
+                {
+                    id = Convert.ToInt32(sr.ReadLine()),
+                    magnitud = Convert.ToDouble(sr.ReadLine()),
+                    fecha = Convert.ToDateTime(sr.ReadLine())
+                };
+                temperaturas.Add(temperatura);
+            }
+            sr.Close();
+            fs.Close();
+        }
+        private void CargarDataGridView()
+        {
+            Datos dato = new Datos();
+            foreach(Temperatura temp in temperaturas)
+            {
+                dato.nombre = departamentos.Find(dep => dep.id == temp.id).nombre;
+                dato.magnitud = temp.magnitud;
+            }
+            datos.Add(dato);
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = datos;
+            dataGridView1.Refresh();
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            LeerDatos();
+            CargarDataGridView();
+        }
+    }
+}
